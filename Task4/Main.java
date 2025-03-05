0012d576087ab8ac781f68e7b453f2c4feea6ed9
Morten Jensen <mo_ten@hotmail.com>
2025-03-05 19:23:57 +0100

    Task4
    Task4 completed

---
+++ Task4/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+//4.e Create a client class, Main with a main method. (You will use this class to test the GameMenu class).
+class Main{
+	
+	public static void main(String[] args) {
+
+        //4.f In the main method create an ArrayList<String> called actions. 
+		ArrayList<String> actions = new ArrayList<>();
+		actions.add("1) Start game");
+		actions.add("2) Resume game");
+		actions.add("3) Pause game");
+		actions.add("4) End game");
+
+		//4.g Still in the main method, instantiate theGameMenu class with the actions list as an argument to the constructor.
+		GameMenu gameMenu = new GameMenu(actions);
+
+        //4.i From the main method in Main test the displayMenu() method by calling it on the GameMenu instance created in step 4.g.
+		gameMenu.displayMenu();
+		
+
+
+
+
+	}
+}
